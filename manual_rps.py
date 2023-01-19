d82b5ee01c66cc16be4dbea512de93d0bdaa6c2e
Mel Jack <melissa_jack@hotmail.co.uk>
2023-01-19 20:17:28 +0000

    Adds a function that will run a game of rock paper scissors taking computer and user choices

--- manual_rps.py
+++ manual_rps.py
@@ -15,3 +15,8 @@ def get_winner(computer_choice, user_choice):
         print("You won!")
     else:
         print("You lost!")
+
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    get_winner(computer_choice, user_choice)
